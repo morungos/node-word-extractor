9327176f5eff8c769516562c3664ce5a2f3ccdd2
Stuart Watt <stuart@morungos.com>
2016-01-14 10:46:27 -0500

    Added a license header for the copied node-old-doc until we can replace with the npm original

--- src/ole-doc.js
+++ src/ole-doc.js
@@ -1,3 +1,30 @@
+// Copyright (c) 2012 Chris Geiersbach
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+// This component as adapted from node-ole-doc, available at:
+// https://github.com/atariman486/node-ole-doc.
+//
+// WARNING: This embedded component will be removed in a future
+// release. It is only included as there are some fixes which
+// are not yet pushed into the npm distribution of node-ole-doc.
+
 var fs = require('fs');
 var EventEmitter = require('events').EventEmitter;
 var util = require('util');
