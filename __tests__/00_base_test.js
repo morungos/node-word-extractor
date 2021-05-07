cf4f5ebcdd2cd9bff2cc982a3991e9209b167b8f
Stuart Watt <stuart@morungos.com>
2021-05-07 18:08:45 -0300

    chore: added a missing file test

--- __tests__/00_base_test.js
+++ __tests__/00_base_test.js
@@ -5,12 +5,20 @@ describe('Checking block from files', () => {
 
   const extractor = new WordExtractor();
 
-  it('should extract a .doc document successfully', () =>
-    extractor.extract(path.resolve(__dirname, "data/test01.doc"))
-  );
+  it('should extract a .doc document successfully', () => {
+    return extractor.extract(path.resolve(__dirname, "data/test01.doc"));
+  });
 
-  it('should extract a .docx document successfully', () =>
-    extractor.extract(path.resolve(__dirname, "data/test13.docx"))
-  );
+  it('should extract a .docx document successfully', () => {
+    return extractor.extract(path.resolve(__dirname, "data/test13.docx"));
+  });
+
+  it('should handle missing file error correctly', () => {
+    const result = extractor.extract(path.resolve(__dirname, "data/missing00.docx"));
+    return expect(result).rejects.toEqual(expect.objectContaining({
+      message: expect.stringMatching(/no such file or directory/)
+    }));
+  });
 
 });
+ 
