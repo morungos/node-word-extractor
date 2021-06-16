3c182621f2e13dd78357c602db421adfcc65fe84
Stuart Watt <stuart@morungos.com>
2021-06-16 20:52:32 -0300

    feat: switched sax parsing to saxes
    resolves #37

--- __tests__/11_openoffice_invalid_xml_test.js
+++ __tests__/11_openoffice_invalid_xml_test.js
@@ -5,11 +5,10 @@ describe('Word file bad-xml.docx', () => {
 
   const extractor = new WordExtractor();
 
-  it.skip('should extract the expected body', () => {
+  it('should extract the expected body', () => {
     return extractor.extract(path.resolve(__dirname, "data/bad-xml.docx"))
       .then((document) => {
-        const value = JSON.stringify(document.getBody());
-        expect(value).toBeInstanceOf(String);
+        expect(document.getBody()).toEqual(expect.stringMatching(/A second test of reviewing, but with Unicode characters in/));
       });
   });
 });
